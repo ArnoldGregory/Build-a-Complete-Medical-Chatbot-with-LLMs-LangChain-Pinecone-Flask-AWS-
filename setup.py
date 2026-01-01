8d5bd3bc6e5576bb8f9e36257c0119961639e50d
ArnoldGregory <omondiarnold06.com>
2026-01-01 11:26:34 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arnold Gregory Omondi",
+    author_email="omondiarnold06@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
